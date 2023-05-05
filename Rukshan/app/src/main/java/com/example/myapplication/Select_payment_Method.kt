a4e7dac920000b5e5694d5ef3cd998dd92d6169a
Anuk87 <anuk08787@gmail.com01~>
2023-05-05 22:39:07 +0530

    UI is done

---
+++ Rukshan/app/src/main/java/com/example/myapplication/Select_payment_Method.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Select_payment_Method : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_payment_method)
+    }
+}
